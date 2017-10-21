96c8bdd1094288a6385e85512aede67ac1124ea0
AbakumovaOlga <31732995+AbakumovaOlga@users.noreply.github.com>
2017-10-21 12:08:27 +0300

    Add files via upload

---
+++ Прогр2/Прогр2/Fish.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Прогр2
+{
+    public abstract class Fish : Animal
+    {
+        protected float startPosX;
+        protected float startPosY;
+
+
+        public abstract void drawAnimal(Graphics g);
+        public abstract void moveAnimal(Graphics g);
+
+        public void setPos(int x, int y)
+        {
+            startPosX = x;
+            startPosY = y;
+        }
+
+        protected int age;
+        public virtual int Age { protected set; get; }
+        public void grow(int a)
+        {
+            if (age + a < Age)
+            {
+                age++;
+            }
+        }
+
+        public virtual double Speed { protected set; get; }
+        public virtual int Weight { protected set; get; }
+        public Color ColorBody { protected set; get; }
+        
+    }
+}
---
+++ Прогр2/Прогр2/TigerShark.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Прогр2
+{
+    public class TigerShark : Shark
+    {
+        private bool bands;
+        private Color dopColor;
+
+        public TigerShark(double speed, int Age, int weight, Color color, bool bands, Color dopColor) : base(speed, Age, weight, color)
+        {
+            this.bands = bands;
+            this.dopColor = dopColor;
+        }
+        protected override void draw1Shark(Graphics g)
+        {
+            base.draw1Shark(g);
+            if (bands)
+            {
+                Pen pen = new Pen(dopColor);
+
+                g.DrawLine(pen, startPosX + Age * 3, startPosY - Weight, startPosX + Age * 4, startPosY);
+                g.DrawLine(pen, startPosX + Age * 6, startPosY - Weight / 2, startPosX + Age * 7, startPosY);
+            }
+
+        }
+    }
+
+
+}
